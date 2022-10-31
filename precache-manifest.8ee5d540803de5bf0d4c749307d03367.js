a746448ac4d9e5f9e1aaf25d670cd82c09236846
Aris <arisverm@gmail.com>
2022-10-31 13:13:26 +0200

    Updates

---
+++ precache-manifest.8ee5d540803de5bf0d4c749307d03367.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b457829df618a375c2f",
+    "url": "/Portofolio-Site/static/css/main.caeac4ad.chunk.css"
+  },
+  {
+    "revision": "8b457829df618a375c2f",
+    "url": "/Portofolio-Site/static/js/main.8b457829.chunk.js"
+  },
+  {
+    "revision": "0d2c0a1a9b3dd51194d1",
+    "url": "/Portofolio-Site/static/js/1.0d2c0a1a.chunk.js"
+  },
+  {
+    "revision": "75b6ed313ca0d54cb604",
+    "url": "/Portofolio-Site/static/js/runtime~main.75b6ed31.js"
+  },
+  {
+    "revision": "8547d2cb8dbdb8ef5a9ff34261ff4d93",
+    "url": "/Portofolio-Site/static/media/coding.8547d2cb.png"
+  },
+  {
+    "revision": "645206e92a5760844f997df6c1bc1319",
+    "url": "/Portofolio-Site/static/media/ArisCv.645206e9.pdf"
+  },
+  {
+    "revision": "ea139316d98facf6f9fb4811c6cdf2e4",
+    "url": "/Portofolio-Site/static/media/topImg.ea139316.png"
+  },
+  {
+    "revision": "6f60e334c3915c2bfe9e1a6496636b3c",
+    "url": "/Portofolio-Site/static/media/skills.6f60e334.png"
+  },
+  {
+    "revision": "54b72fe3750c476aa651f4a5433280fe",
+    "url": "/Portofolio-Site/static/media/kit.54b72fe3.png"
+  },
+  {
+    "revision": "1817a2b47f51e76d69217c3dd30b2674",
+    "url": "/Portofolio-Site/static/media/vg.1817a2b4.png"
+  },
+  {
+    "revision": "815f991debe2b5699c13d10f0c45b269",
+    "url": "/Portofolio-Site/static/media/tin.815f991d.png"
+  },
+  {
+    "revision": "9e78dcc42d5fda0001e245a10d63a065",
+    "url": "/Portofolio-Site/static/media/netflix.9e78dcc4.png"
+  },
+  {
+    "revision": "133887ca0eef3342638574e20910d4ac",
+    "url": "/Portofolio-Site/index.html"
+  }
+];
